943560efa1d90a3514dfd3c7b37f83df069848de
Andrew <telepnev.1997@mail.ru>
2024-02-21 10:45:10 +0700

    Initial commit

---
+++ src/main/java/com/example/GB_Eml1Sem7SpringCore/GbEml1Sem7SpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.GB_Eml1Sem7SpringCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbEml1Sem7SpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbEml1Sem7SpringCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GB_Eml1Sem7SpringCore/GbEml1Sem7SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GB_Eml1Sem7SpringCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbEml1Sem7SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
